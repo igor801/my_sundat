4498f7791aab4dddbc0bd46fe48b383854425f42
igorbabikov <babikov_igor@inbox.ru>
2025-04-28 16:03:44 +0300

    отправка через идеа 2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("is not 3th commit");
+        System.out.println("izmenenie");
 
     }
 }
